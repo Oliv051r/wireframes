2535fdc2a1da60264b25c2f617285bc6811eca91
“indi0203” <indi0203@stud.kea.dk>
2021-10-14 13:11:17 +0200

    lighthouse

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ const highlights = document.querySelectorAll(".text-highlight");
 highlights.forEach((highlight) => {
   ScrollTrigger.create({
     trigger: highlight,
+    markers: true,
     start: "-90px center",
     onEnter: () => highlight.classList.add("active"),
     onEnterBack: () => highlight.classList.remove("active"),
